ee2a433595528b011384c9f7a7bbe0455989d6ea
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-01-26 17:06:35 +0200

    Create 06_recursive_power.py

---
+++ Advanced/09_functions_advanced_lab/06_recursive_power.py
@@ -0,0 +1,11 @@
+def recursive_power(number, power):
+    if power == 1:
+        return number
+    elif power == 0:
+        return 1
+    else:
+        return number * recursive_power(number, power - 1)
+
+
+print(recursive_power(2, 10))
+print(recursive_power(10, 100))
