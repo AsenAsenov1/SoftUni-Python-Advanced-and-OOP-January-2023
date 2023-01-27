2660d51644ba9750ae8a934292f271665959af99
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-01-27 13:37:02 +0200

    Create 05_concatenate.py

---
+++ Advanced/10_functions_advanced_exercise/05_concatenate.py
@@ -0,0 +1,13 @@
+def concatenate(*args, **kwargs):
+    text = "".join(args)
+    for key, value in kwargs.items():
+        if key in text:
+            text = text.replace(key, value)
+    return text
+
+
+print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
+# Output: SoftUniIsGreat!
+
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
+# Output: I Love Python
