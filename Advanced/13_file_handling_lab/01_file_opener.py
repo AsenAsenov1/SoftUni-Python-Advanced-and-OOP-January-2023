04137849de510f48ee92d04975f0f5bb92946908
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-02-02 12:16:41 +0200

    Update 01_file_opener.py

--- Advanced/13_file_handling_lab/01_file_opener.py
+++ Advanced/13_file_handling_lab/01_file_opener.py
@@ -1,3 +1,9 @@
+"""
+You are given a file called text.txt with the following text:
+
+Create a program that opens the file. If the file is found, print 'File found'. If the file is not found, print 'File not found'.
+"""
+
 import os
 
 path = "./text.txt"
