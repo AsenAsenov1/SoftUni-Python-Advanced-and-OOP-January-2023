f75ebfb9fb0c781b60ddc6cf2362765de1d6df8e
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-02-09 12:48:45 +0200

    Create 03_triangle.py

---
+++ Advanced/17_modules_lab/03_triangle.py
@@ -0,0 +1,20 @@
+"""
+Create a module for printing a triangle. You will receive an integer number which is the size of the triangle.
+Input: 5
+Output:
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+"""
+
+from triangle import create_triangle
+
+create_triangle(5)
