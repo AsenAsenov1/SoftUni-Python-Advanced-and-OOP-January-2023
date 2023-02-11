479d844a59ed1c66a978863483c14b4afce17471
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-02-11 17:01:34 +0200

    Update __init__.py

--- Advanced/17_modules_lab/operations/__init__.py
+++ Advanced/17_modules_lab/operations/__init__.py
@@ -1,4 +1,12 @@
-def calculate(num1, operator, num2):
-    operations = {
-    }
-    ...
+def calculate(*args):
+    num1, operator, num2 = args
+
+    operations = {"/": lambda a, b: a / b,
+                  "*": lambda a, b: a * b,
+                  "-": lambda a, b: a - b,
+                  "+": lambda a, b: a + b,
+                  "^": lambda a, b: a ** b,
+                  }
+    result = operations[operator](float(num1), int(num2))
+
+    return f"{result:.2f}"
