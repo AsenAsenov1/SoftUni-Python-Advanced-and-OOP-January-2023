43fe506586b0c6b69a3a80d3da93952e503802f5
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-02-20 22:51:40 +0200

    Create 02_scope_mess.py

---
+++ OOP/01_first_steps_in_oop_lab/02_scope_mess.py
@@ -0,0 +1,23 @@
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        global x
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+
+x = "global"
+
+print(x)
+outer()
+print(x)
