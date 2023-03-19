aa97c4b42fe5ce4c7b6b87cb57ef41791b0d8922
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2023-03-19 09:14:09 +0200

    Create SOLID exercise

---
+++ OOP/14_solid_exercise/shapes.py
@@ -0,0 +1,69 @@
+from abc import abstractmethod, ABC
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def calculate_area(self):
+        ...
+
+
+class Rectangle(Shape):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+
+class Triangle(Shape):
+
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        return self.base * self.height / 2
+
+
+class Square(Shape):
+
+    def __init__(self, side):
+        self.side = side
+
+    def calculate_area(self):
+        return self.side * self.side
+
+
+class AreaCalculator:
+
+    def __init__(self, shapes):
+        self.shapes = shapes
+
+    @property
+    def shapes(self):
+        return self.__shapes
+
+    @shapes.setter
+    def shapes(self, value):
+        if not isinstance(shapes, list):
+            raise ValueError("`shapes` should be of type `list`.")
+
+        self.__shapes = value
+
+    @property
+    def total_area(self):
+        total = 0
+
+        for shape in self.shapes:
+            total += shape.calculate_area()
+
+        return total
+
+
+shapes = [Rectangle(1, 6), Triangle(2, 3), Square(3)]
+calculator = AreaCalculator(shapes)
+
+print("The total area is: ", calculator.total_area)
